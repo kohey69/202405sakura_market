44d0159014cf530a68a824d82d711301328768f2
kuma <kuma@sonicgarden.jp>
2024-05-23 11:35:48 +0900

    feat: LPページ作成

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,8 @@ module PracticeSakuraApp
       g.test_framework :rspec,
                        view_specs: false,
                        helper_specs: false,
-                       routing_specs: false
+                       routing_specs: false,
+                       request_specs: false
     end
 
     # Don't generate system test files.
