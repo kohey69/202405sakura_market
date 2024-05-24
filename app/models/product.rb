5a84db99a35868041d1d2465261724e2bc0db6f8
kuma <kuma@sonicgarden.jp>
2024-05-24 15:32:31 +0900

    feat: 公開フラグを表示する

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
   scope :default_order, -> { order(position: :asc, created_at: :desc) }
+  scope :published, -> { where(published: true) }
 
   def tax
     (self.price * PRODUCT_TAX_RATE).floor
