e8a71cb9e6aa269cec739aa02d8b1432f30c57c7
kuma <kuma@sonicgarden.jp>
2024-05-24 13:50:31 +0900

    feat: productのfactory_bot作成

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :product do
-    
+    name { 'いちご' }
+    price { '400' }
+    description { '甘いいちご。芳醇な香りとこく。' }
+    published { true }
+
+    trait :unpublished do
+      published { false }
+    end
   end
 end
