7944306b0c2da8424ee4fec0f05d1b45075dec24
kuma <kuma@sonicgarden.jp>
2024-05-26 16:44:21 +0900

    feat: 消費税額のテスト追加

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#tax' do
+    it '税率が8%で小数点以下の金額が切り捨てられていること' do
+      product = create(:product, price: '110')
+      expect(product.tax).not_to eq 8.8
+      expect(product.tax).to eq 8
+    end
+  end
+
+  describe '#price_with_tax' do
+    it '消費税込みの金額が返されること' do
+      product = create(:product, price: '110')
+      expect(product.price_with_tax).to eq 118
+    end
+  end
 end
