bc0d77ba9f0f37eda2d91fb3d071f705d42afcc3
kuma <kuma@sonicgarden.jp>
2024-05-26 16:24:30 +0900

    feat: 商品登録の時、画像を必須に

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  config.file_fixture_path = Rails.root.join('spec/fixtures/files')
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
