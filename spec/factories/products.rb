3d7123ee418418276ba55a49070c24afe484cff8
kuma <kuma@sonicgarden.jp>
2024-05-26 16:36:59 +0900

    fix: factoryの画像uploadがうまくいかなかったのでRack::Test::UploadedFile.newを利用する

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { 'いちご' }
     price { '400' }
     description { '甘いいちご。芳醇な香りとこく。' }
-    image { ActiveStorageTestHelper.create_file_blob_by_path(Rails.root.join('spec/fixtures/files/images/strawberry.jpg')) }
+    image { Rack::Test::UploadedFile.new('spec/fixtures/files/images/strawberry.jpg') }
     published { true }
 
     trait :unpublished do
