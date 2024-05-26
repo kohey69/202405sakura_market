77e2c06bad5b73afc092fd00298b6acd8011dad6
kuma <kuma@sonicgarden.jp>
2024-05-26 16:23:14 +0900

    fix: id降順に

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     name { 'いちご' }
     price { '400' }
     description { '甘いいちご。芳醇な香りとこく。' }
+    image { ActiveStorageTestHelper.create_file_blob_by_path(Rails.root.join('spec/fixtures/files/images/strawberry.jpg')) }
     published { true }
 
     trait :unpublished do
