846b197ca34bfaced660bec9a26b13a9ce104dc6
kuma <kuma@sonicgarden.jp>
2024-05-28 17:43:01 +0900

    feat: 配送先住所のテスト追加

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :address do
-    name { Faker::Name.name }
+    user
+    name { '熊谷 康平' }
+    postal_code { '888-8888' }
+    prefecture { '兵庫県' }
+    city { '中央区雲井通' }
+    other_address { '3-1-8 ソニックレジデンス808号室' }
+    phone_number { '090-xxxx-xxxx' }
   end
 end
