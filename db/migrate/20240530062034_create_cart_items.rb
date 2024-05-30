2a747f5da074c41eba7d21a69abdad9a14726ac5
kuma <kuma@sonicgarden.jp>
2024-05-30 16:46:40 +0900

    feat: cart/cart_itemモデル作成

---
+++ db/migrate/20240530062034_create_cart_items.rb
@@ -0,0 +1,11 @@
+class CreateCartItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :cart_items do |t|
+      t.references :cart, null: false, foreign_key: true # dependent: :destroyにする
+      t.references :product, null: false, foreign_key: true
+      t.integer :quantity, null: false, default: 1
+      t.index %i[cart_id product_id], unique: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_061303) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_062034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,24 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_061303) do
     t.index ["email"], name: "index_administrators_on_email", unique: true
   end
 
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id", "product_id"], name: "index_cart_items_on_cart_id_and_product_id", unique: true
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.integer "price", null: false
@@ -89,4 +107,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_061303) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
 end
