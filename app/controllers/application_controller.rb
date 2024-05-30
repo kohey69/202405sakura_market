d3c2a9d18b556b27f495967aab995e88fa066608
kuma <kuma@sonicgarden.jp>
2024-05-30 16:46:59 +0900

    feat: current_cart実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_cart
+
+  def current_cart
+    if user_signed_in?
+      current_user.cart
+    elsif session[:cart_id].present?
+      Cart.find(session[:cart_id])
+    else
+      cart = Cart.create!
+      session[:cart_id] = cart.id
+      cart
+    end
+  end
 end
