83ad9777f734d3e804aec8b286267c564f99c33a
kuma <kuma@sonicgarden.jp>
2024-05-31 21:29:02 +0900

    refactor: sessionでcartが見つからなかった場合の処理を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,16 @@ class ApplicationController < ActionController::Base
   helper_method :current_cart
 
   def current_cart
-    if user_signed_in?
-      current_user.cart
-    elsif session[:cart_id].present?
-      Cart.find(session[:cart_id])
-    else
-      cart = Cart.create!
-      session[:cart_id] = cart.id
-      cart
-    end
+    return current_user.cart if user_signed_in?
+
+    Cart.find_by(id: session[:cart_id]) || create_cart_with_session
+  end
+
+  private
+
+  def create_cart_with_session
+    cart = Cart.create!
+    session[:cart_id] = cart.id
+    cart
   end
 end
