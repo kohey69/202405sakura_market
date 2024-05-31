2a6177ef248c591fc48a324e415fc98e794ea0fc
kuma <kuma@sonicgarden.jp>
2024-05-31 14:26:08 +0900

    refactor: ユニーク制約をつける

--- db/migrate/20240528061303_create_addresses.rb
+++ db/migrate/20240528061303_create_addresses.rb
@@ -1,12 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[7.1]
   def change
     create_table :addresses do |t|
-      t.references :user
+      t.references :user, null: false, foreign_key: true, index: { unique: true }
       t.string :name, null: false, default: '', comment: '宛名'
       t.string :postal_code, null: false, default: ''
       t.string :prefecture, null: false, default: ''
       t.string :city, null: false, default: ''
-      t.string :other_address, null: false, default: ''
+      t.string :other_address, null: false, default: '', comment: '丁目・番地・建物名'
       t.string :phone_number, null: false, default: ''
       t.timestamps
     end
