165a135bceb8554f8528f23b1e0bd36ad9d4fef6
kuma <kuma@sonicgarden.jp>
2024-06-03 19:02:58 +0900

    fix: active_job.queue_adapterを:testに設定しておく

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -55,6 +55,8 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
+  # rails7.1のissue https://github.com/rspec/rspec-rails/issues/2697
+  config.active_job.queue_adapter = :test
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
