9d2b66fb063f83a9b8791b3974ff70b93c686049
kuma <kuma@sonicgarden.jp>
2024-06-04 11:14:13 +0900

    feat: カート詳細画面追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,7 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
-  validates :quantity, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :product_id, uniqueness: { scope: :cart_id }
 
   scope :default_order, -> { order(id: :desc) }
