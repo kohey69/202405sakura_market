782c75f7ab4e0624578bd968e06882c5c717816b
kuma <kuma@sonicgarden.jp>
2024-06-06 17:02:15 +0900

    feat: purchase_itemを同時に作成し、購入時にカートアイテムを削除するように

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -11,6 +11,8 @@ class Purchase < ApplicationRecord
   validates :city, presence: true
   validates :other_address, presence: true
 
+  after_create :destroy_cart_items!
+
   def assign_cart_attributes
     self.total_payment = user.cart.total_payment
     self.total_price = user.cart.total_price
@@ -19,10 +21,19 @@ class Purchase < ApplicationRecord
     self.shipping_fee = user.cart.shipping_fee
   end
 
-  def save_with_destroy_cart_items!
+  def save_with_purchase_items!
     transaction do
+      self.user.cart.cart_items.each do |cart_item|
+        purchase_item = self.purchase_items.build
+        purchase_item.product_name = cart_item.product.name
+        purchase_item.product_id = cart_item.product_id
+        purchase_item.quantity = cart_item.quantity
+      end
       self.save!
-      self.user.cart.cart_items.destroy_all
     end
+  end
+
+  def destroy_cart_items!
+    self.user.cart.cart_items.destroy_all
   end
 end
