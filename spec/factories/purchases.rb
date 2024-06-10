29ab3a1755c501d77fc2c27b4002833acaa8549b
kuma <kuma@sonicgarden.jp>
2024-06-10 15:59:38 +0900

    feat: 配送希望日時を指定できるように

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -6,6 +6,8 @@ FactoryBot.define do
     total_tax { 100 }
     cod_fee { 100 }
     shipping_fee { 100 }
+    delivery_on { Date.current + 4.days }
+    delivery_time_slot { 'from_8_to_12' }
     address_name { Faker::Name.name }
     postal_code { '651-0096' }
     prefecture { '兵庫県' }
