df71c8db26faea7bd62cfa16d3cb5e131dfe620a
kuma <kuma@sonicgarden.jp>
2024-06-17 10:21:11 +0900

    refactor: validation追加

--- app/models/purchase_item.rb
+++ app/models/purchase_item.rb
@@ -2,6 +2,6 @@ class PurchaseItem < ApplicationRecord
   belongs_to :purchase
   belongs_to :product
 
-  validates :quantity, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :product_name, presence: true
 end
