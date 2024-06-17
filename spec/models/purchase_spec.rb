1fa9c4cd0a9b281912c8788d1ec555250aad73e0
kuma <kuma@sonicgarden.jp>
2024-06-17 13:57:35 +0900

    feat: 購入処理の途中で商品が非公開になった場合を考慮するテスト作成

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,37 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  describe '#purchase_and_destroy_cart_items!' do
+  describe '#build_with_purchase_items' do
     let(:user) { create(:user) }
     let(:product) { create(:product, name: 'いちご') }
-    let(:purchase) do
-      build(:purchase, user:,
-                       total_payment: 1100,
-                       total_price: 800,
-                       total_tax: 100,
-                       cod_fee: 100,
-                       shipping_fee: 100,
-                       address_name: '田中太郎',
-                       postal_code: '651-0096',
-                       prefecture: '兵庫県',
-                       city: '神戸市中央区雲井通',
-                       other_address: '3-2-5',
-                       phone_number: '090-xxxx-xxxx')
+    let(:params) do
+      {
+        address_name: '田中太郎',
+        postal_code: '651-0096',
+        prefecture: '兵庫県',
+        city: '神戸市中央区雲井通',
+        other_address: '3-2-5',
+        phone_number: '090-xxxx-xxxx',
+        delivery_on: '2024-06-17',
+        delivery_time_slot: 'from_8_to_12',
+      }
     end
 
     before do
+      travel_to '2024-06-01'
       create(:cart_item, product:, cart: user.cart, quantity: 3)
-      purchase.purchase_and_destroy_cart_items!
     end
 
     it '購入明細が同時に作成されること' do
-      expect(purchase.purchase_items.count).to eq 1
+      purchase = user.purchases.build_with_purchase_items(params)
+      purchase.save_with_destroy_cart_items
+
+      expect(purchase.reload.purchase_items.count).to eq 1
       expect(purchase.purchase_items.first.product_name).to eq 'いちご'
       expect(purchase.purchase_items.first.quantity).to eq 3
+      expect(user.cart.cart_items.count).to eq 0
     end
 
-    it 'カート内アイテムがクリアされること' do
-      expect(user.cart.cart_items.count).to eq 0
+    it '購入処理の途中で商品が非公開にされると購入処理がロールバックされること' do
+      purchase = user.purchases.build_with_purchase_items(params)
+      product.update!(published: false)
+      purchase.save_with_destroy_cart_items
+
+      expect(purchase.persisted?).to eq false
+      expect(user.cart.reload.cart_items.count).to eq 1
     end
   end
 
