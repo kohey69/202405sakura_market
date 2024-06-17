380ff9c534c1e1efcf71d778de331a23b19fcbb9
kuma <kuma@sonicgarden.jp>
2024-06-17 13:57:26 +0900

    feat: 購入処理の途中で商品が非公開になった場合を考慮する

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -58,6 +58,7 @@ SimpleForm.setup do |config|
     b.use :label, class: 'form-label'
     b.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
     b.use :full_error, wrap_with: { class: 'invalid-feedback' }
+    b.use :error, wrap_with: { class: 'invalid-feedback' }
     b.use :hint, wrap_with: { class: 'form-text' }
   end
 
