e80267fd235a5c6a7473d9630548cd4f263490d3
kuma <kuma@sonicgarden.jp>
2024-06-17 10:17:14 +0900

    refactor: 不要なコメント削除

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,12 +3,6 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
   def create
     super
 
@@ -21,14 +15,8 @@ class Users::SessionsController < Devise::SessionsController
     session[:cart_id] = nil
   end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
   # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
